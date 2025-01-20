2a1c8f554b8b6af1b98bd27ca9f49a6c1e8480fd
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2025-01-20 11:51:00 +0100

    Add copy/paste keymaps to system clipboard

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,3 +3,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+-- Copy/Paste from system clipboard
+vim.keymap.set('n', '<leader>Y', '"+Y', {})
+vim.keymap.set('n', '<leader>P', '"+P', {})
