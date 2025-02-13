cb9a47683586ffbb8db297e1eacb1ca1b76546bc
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2025-02-13 19:20:50 +0100

    Update autocomplete to use buf lsp

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -12,16 +12,24 @@ return {
 
   {
     "hrsh7th/nvim-cmp",
+    dependecies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      'saadparwaiz1/cmp_luasnip',
+      "L3MON4D3/LuaSnip",
+    },
     config = function()
       -- Set up nvim-cmp.
       local cmp = require 'cmp'
-      require("luasnip.loaders.from_vscode").lazy_load()
+      local luasnip = require("luasnip")
+      --require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
         snippet = {
           -- REQUIRED - you must specify a snippet engine
           expand = function(args)
-            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            luasnip.lsp_expand(args.body) -- For `luasnip` users.
           end,
         },
         window = {
@@ -35,14 +43,37 @@ return {
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
           ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end,
+            { 'i', 's'}),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
         }),
         sources = cmp.config.sources({
-          -- { name = 'nvim_lsp' },
-          -- { name = 'vsnip' }, -- For vsnip users.
+          { name = 'nvim_lsp' },
           { name = 'luasnip' }, -- For luasnip users.
-        }, {
           { name = 'buffer' },
+          { name = 'path' },
+          --{ name = 'vsnip' }, -- For vsnip users.
         })
+        --[[ {
+          { name = 'buffer' },
+        }
+        ]] --
       })
     end
   }
