423eee6165131932ae521097d0b3788424e0aef1
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2025-04-05 23:53:24 +0200

    Add moving pane split line keymaps

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -7,3 +7,9 @@ vim.cmd("set shiftwidth=2")
 -- Copy/Paste from system clipboard
 vim.keymap.set('n', '<leader>Y', '"+Y', {})
 vim.keymap.set('n', '<leader>P', '"+P', {})
+
+-- Move pane split line
+vim.keymap.set('n', '<leader><Up>', ':resize -5<CR>', {silent = true})
+vim.keymap.set('n', '<leader><Down>', ':resize +5<CR>', {silent = true})
+vim.keymap.set('n', '<leader><Left>', ':vertical resize -5<CR>', {silent = true})
+vim.keymap.set('n', '<leader><Right>', ':vertical resize +5<CR>', {silent = true})
