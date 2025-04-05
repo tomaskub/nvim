c849456e56a6d7ca62083eef72fe56c1cc872fdd
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2025-04-05 23:53:53 +0200

    Add switching lualine colorscheme on change of theme

--- lua/plugins/autodarkmode.lua
+++ lua/plugins/autodarkmode.lua
@@ -5,9 +5,15 @@ return {
     update_interval = 1000,
     set_dark_mode = function()
       vim.cmd.colorscheme("catppuccin-mocha")
+      require('lualine').setup({
+        options = { theme = 'dracula' }
+      })
     end,
     set_light_mode = function()
       vim.cmd.colorscheme("catppuccin-latte")
+      require('lualine').setup({
+        options = { theme = 'gruvbox_light'}
+      })
     end
   }
 }
