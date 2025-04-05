4144792bfa107b4ab71f1ce9ae8182202cdfbc80
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2025-04-05 23:55:25 +0200

    Add nvim-lspconfig

---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,43 @@
+return {
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    { "antosha417/nvim-lsp-file-operations", config = true },
+  },
+  config = function()
+    local lspconfig = require("lspconfig")
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    local capabilities = cmp_nvim_lsp.default_capabilities()
+    local opts = { noremap = true, silent = true }
+    local on_attach = function(_, bufnr)
+      opts.buffer = bufnr
+
+      opts.desc = "Show line diagnostics"
+      vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+
+      opts.desc = "Show documentation for what is under cursor"
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+      opts.desc = "Show LSP definition"
+      vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions trim_text=true<cr>", opts)
+    end
+
+    lspconfig["sourcekit"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+      on_init = function(client)
+        -- HACK: to fix some issues with LSP
+        -- more details: https://github.com/neovim/neovim/issues/19237#issuecomment-2237037154
+        client.offset_encoding = "utf-8"
+      end,
+    })
+
+    -- nice icons
+    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    end
+  end,
+}
