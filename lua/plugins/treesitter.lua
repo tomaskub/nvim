d31b311b5e948eadaf46896e95b61a073f281639
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2025-04-05 23:58:13 +0200

    Add running treesitter on bufread and bufnewfile with ensure installed most popular components

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,25 @@
-return { 
+return {
   "nvim-treesitter/nvim-treesitter",
+  event = { 'BufReadPre', 'BufNewFile' },
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup(
-    {
+      {
+        incremental_selection = { enable = false },
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
-    }
+        autotag = { enable = false },
+        ensure_installed = {
+          'json',
+          'yaml',
+          'markdown',
+          'markdown_inline',
+          'lua',
+          'gitignore',
+          'swift',
+        },
+      }
     )
   end
 }
