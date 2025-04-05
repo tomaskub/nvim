ee28d2cee626c9c2cf030b8715451cb52e7ad881
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2025-04-05 23:57:33 +0200

    Add desc to telescope keybinds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,8 +7,18 @@ return {
     },
     config = function()
       local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set(
+        'n',
+        '<leader>ff',
+        builtin.find_files,
+        { desc = "Fuzzy find file in cwd" }
+      )
+      vim.keymap.set(
+        'n',
+        '<leader>fg',
+        builtin.live_grep,
+        { desc = "Live grep in cwd" }
+      )
     end
   },
 
