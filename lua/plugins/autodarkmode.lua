b38fc8b448c5b06c477286cd1802cbe067f2256f
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2025-06-22 09:04:55 +0200

    Add markdown colors update on theme change

--- lua/plugins/autodarkmode.lua
+++ lua/plugins/autodarkmode.lua
@@ -1,19 +1,65 @@
+local function set_render_markdown_higlights(mode)
+  local fg_colors = {}
+  local bg_color = {}
+
+  if mode == 'light' then 
+    fg_colors = {
+      '#458588', -- Blue   (H1)
+      '#b16286', -- Purple (H2)
+      '#689d6a', -- Aqua   (H3)
+      '#d65d0e', -- Orange (H4)
+      '#d79921', -- Yellow (H5)
+      '#98971a', -- Green  (H6)
+    }
+    bg_color = '#f9f5d7'
+  else 
+    fg_colors = {
+      '#cba6f7', -- Mauve    (H1)
+      '#f5c2e7', -- Pink     (H2)
+      '#a6e3a1', -- Green    (H3)
+      '#f9e2af', -- Yellow   (H4)
+      '#94e2d5', -- Teal     (H5)
+      '#f2cdcd', -- Flamingo (H6)
+    }
+    bg_color = '#1e1e2e'
+  end 
+
+  -- Set foregrounds
+  for i = 1, 6 do
+    local hl_group_fg = 'RenderMarkdownH' .. i .. 'Fg'
+    vim.api.nvim_set_hl(0, hl_group_fg, { fg = fg_colors[i], bold = true })
+  end 
+
+  -- Set background
+  for i = 1, 6 do
+    local hl_group_fg = 'RenderMarkdownH' .. i .. 'Bg'
+    vim.api.nvim_set_hl(0, hl_group_bg, { bg = bg_color })
+  end 
+end 
+
 return {
   "f-person/auto-dark-mode.nvim",
   lazy = false,
+  priority = 1001, -- ensure run after catppuccin
   opts = {
     update_interval = 1000,
+
     set_dark_mode = function()
+      -- vim.o.background = 'dark'
       vim.cmd.colorscheme("catppuccin-mocha")
       require('lualine').setup({
         options = { theme = 'dracula' }
       })
+      --set_render_markdown_highlights("dark")
     end,
+
     set_light_mode = function()
+      --vim.o.background = 'light'
       vim.cmd.colorscheme("catppuccin-latte")
       require('lualine').setup({
-        options = { theme = 'gruvbox_light'}
+        options = { theme = "onelight"}
       })
+      --set_render_markdown_higlights("light")
     end
   }
 }
--- lua/plugins/render-markdown.nvim.lua
+++ lua/plugins/render-markdown.nvim.lua
@@ -3,8 +3,38 @@ return {
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
-      opts = {
-        file_types = { "markdown", "Avante" },
+  opts = {
+    file_types = { "markdown", "Avante" },
+    -- You can add other render-markdown options here if needed
+    heading = {
+      position = 'inline',
+      border = false,
+      -- Define the names of the highlight groups to use for headings
+      foregrounds = {
+        'RenderMarkdownH1Fg',
+        'RenderMarkdownH2Fg',
+        'RenderMarkdownH3Fg',
+        'RenderMarkdownH4Fg',
+        'RenderMarkdownH5Fg',
+        'RenderMarkdownH6Fg',
       },
-      ft = { "markdown", "Avante" },
+      backgrounds = {
+        'RenderMarkdownH1Bg',
+        'RenderMarkdownH2Bg',
+        'RenderMarkdownH3Bg',
+        'RenderMarkdownH4Bg',
+        'RenderMarkdownH5Bg',
+        'RenderMarkdownH6Bg',
+      },
+    },
+  },
+  ft = { "markdown", "Avante" },
+
+  -- We keep the config function to ensure setup is called,
+  -- but the highlight definitions are moved to auto-dark-mode.
+  config = function(_, opts)
+
+    require('render-markdown').setup(opts)
+
+  end,
 }
