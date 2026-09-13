1641f61f8dfd24b56f87e32e3d90fac81e658c73
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2026-09-13 16:25:39 +0200

    Fix failing markdown renderer

---
+++ queries/markdown/injections.scm
@@ -0,0 +1,28 @@
+; Neovim 0.12 passes directive captures as lists of nodes. The archived
+; nvim-treesitter master query uses an incompatible custom directive for
+; fenced-code languages, so use Neovim's native injection capture instead.
+(fenced_code_block
+  (info_string
+    (language) @injection.language)
+  (code_fence_content) @injection.content)
+
+((html_block) @injection.content
+  (#set! injection.language "html")
+  (#set! injection.combined)
+  (#set! injection.include-children))
+
+((minus_metadata) @injection.content
+  (#set! injection.language "yaml")
+  (#offset! @injection.content 1 0 -1 0)
+  (#set! injection.include-children))
+
+((plus_metadata) @injection.content
+  (#set! injection.language "toml")
+  (#offset! @injection.content 1 0 -1 0)
+  (#set! injection.include-children))
+
+([
+  (inline)
+  (pipe_table_cell)
+] @injection.content
+  (#set! injection.language "markdown_inline"))
